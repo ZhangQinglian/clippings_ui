65514f0e5f97be2d7d7bf61bc3b278d2c1f21e93
Scott ZHANG <zqlxtt@live.com>
2017-02-23 09:44:31 +0800

    add bottom bar

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # clippings_ui
+
+## 技术支持
+- [BottomBar](https://github.com/roughike/BottomBar)
+- [RxJava](https://github.com/ReactiveX/RxJava)
+- [RxAndroid](https://github.com/ReactiveX/RxAndroid/wiki)
+- [DataBinding](https://developer.android.com/topic/libraries/data-binding/index.html)
--- src/main/java/com/zql/android/clippings/MainActivity.java
+++ src/main/java/com/zql/android/clippings/MainActivity.java
@@ -1,17 +1,64 @@
 package com.zql.android.clippings;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
-import io.reactivex.Flowable;
-import io.reactivex.functions.Consumer;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
+import com.zql.android.clippings.sdk.parser.Clipping;
+import com.zql.android.clippings.sdk.parser.ClippingsParser;
+import com.zql.android.clippings.sdk.provider.ClippingContract;
+import com.zql.android.clippings.sdk.provider.ClippingsProvider;
+import com.zqlite.android.logly.Logly;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BottomBar mBottomBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Logly.setGlobalTag(new Logly.Tag(Logly.FLAG_THREAD_NAME, "Clippings", Logly.DEBUG));
         setContentView(R.layout.activity_main);
+
+        mBottomBar = (BottomBar) findViewById(R.id.main_bottom_bar);
+        mBottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+            }
+        });
+    }
+
+    private void testQuery() {
+        ContentResolver contentResolver = getContentResolver();
+        Cursor cursor = contentResolver.query(ClippingContract.CLIPPINGS_URI, ClippingContract.PROJECTION_CLIPPINGS_ALL, null, null, null);
+        if (cursor != null) {
+            try {
+
+                while (cursor.moveToNext()) {
+                    Clipping clipping = Clipping.getInstance(cursor);
+                    Logly.d(clipping.toString());
+                }
+
+            } catch (Exception e) {
+
+            } finally {
+                cursor.close();
+            }
+        }
+
+    }
+
+    private void initDatabase() {
+        try {
+            ClippingsParser.own().parse(getAssets().open("My Clippings.txt"), this);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
